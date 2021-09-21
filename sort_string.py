3bdd5765c16a2dd3e3430db842a6f4c031d0ed86
tousif-1 <tousifahamedan@gmail.com>
2021-09-21 13:01:46 +0530

    Creating Sort string function

---
+++ sort_string.py
@@ -0,0 +1,16 @@
+# Sort any given string with Case sensitive result
+# Using sort function, python sorts with Capital letter string first
+# So convert string to lower case and add to starting of string
+# for example: yellow car TRUNK
+# yellowyello, carcar, trunkTRUNK
+# then we use sort function to sort this
+# are remove the initial lower case string.
+
+
+
+def sort_words(input):
+  words = inputs.split()
+  words = [w.lower() + w for w in words]
+  words.sort()
+  words = [w[len(w)//2:] for w in words]
+  return ' '.join(words)
