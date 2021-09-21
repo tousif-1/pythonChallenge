ce238698adffa4998d6d74680e252e9341e7b799
tousif-1 <tousifahamedan@gmail.com>
2021-09-22 00:13:57 +0530

    Create buildZip.py

---
+++ buildZip.py
@@ -0,0 +1,16 @@
+# input directory path list of file extensions output file path
+#maintian folder structure
+
+import os
+from zipfile import ZipFile
+
+def zip_all(search_dir, extension_list, output_path):
+    with ZipFile(output_path, 'w') as output_zip:
+        for root, dir, files in os.walk(search_dir):
+            rel_path = os.path.relpath(root, search_dir)
+            for file in files:
+                name, ext = os.path.splitext(file)
+                if ext.lower() in extension_list:
+                    output_zip.write(os.path.join(root, file), arcname=os.path.join(rel_path, file))
+
+zip_all('C:\\user\\Desktop\\archs', ['.png', '.txt'], 'arch.zip')
